0d7eede121d1749e0e50a465aab5d53ed2dd8cd5
Yamil <ycastroa@gmu.edu>
2021-06-09 19:00:55 -0400

    Examples

--- Main.go
+++ Main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	_  = iota // i dont care what value is discard
+	KB = 1 << (10 * iota)
+	MG
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YT
+	y       = iota // counter 0
+	x int16 = 27   //counter 1
+	z       = iota //counter 2
+)
 
 func main() {
-	fmt.Println("hello go!")
+	var (
+		actorName string = "one artist"
+		companion string = "second actor"
+	)
+	fmt.Println(actorName, companion)
+	var i int = 42
+	fmt.Printf("%v, %T,\n", i, i)
+
+	var o string
+	o = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", o, o)
+
+	// var n bool =true
+	// fmt.Printf("%v,%T\n",n,n)
+
+	n := 1 == 1
+	m := 1 == 2
+	fmt.Printf("%v,%T\n", n, n)
+	fmt.Printf("%v,%T\n", m, m)
+
+	var c uint16 = 42
+	fmt.Printf("%v,%T\n", c, c)
+
+	a := 10             //1010
+	b := 3              //0011
+	fmt.Println(a & b)  //0010
+	fmt.Println(a | b)  //1011
+	fmt.Println(a ^ b)  //1001  if they have one bit set
+	fmt.Println(a &^ b) //0100 if they have
+
+	d := 8
+	fmt.Println(d << 3) // 2^4 * 2^3 = 2^6
+	fmt.Println(d >> 3) //2^3 / 2^3 = 2^0
+
+	var e float64 = 3.14
+	e = 13.7e72
+	fmt.Println(e)
+
+	var f complex64 = 1 + 2i
+	fmt.Printf("%v, %T\n", f, f)
+	fmt.Printf("%v, %T\n", real(f), real(f))
+	fmt.Printf("%v, %T\n", imag(f), imag(f))
+
+	var g complex128 = complex(5, 12)
+	fmt.Printf("%v, %T\n", g, g)
+	fmt.Printf("%v, %T\n", real(g), real(g))
+	fmt.Printf("%v, %T\n", imag(g), imag(g))
+
+	h := "This is a string"
+	//h[2] ="u" you cant
+	fmt.Printf("%v,%T\n", h[2], h[2])
+
+	j := "this is a string"
+	j2 := "this is also a string"
+	fmt.Printf("%v,%T\n", j+j2, j+j2)
+
+	k := "this is string"
+	l := []byte(k)
+	fmt.Printf("%v,%T\n", l, l)
+	fmt.Println(len(k), " arr: ", len(l))
+
+	r := 'a'
+	fmt.Printf("%v,%T\n", r, r)
+	var r1 rune = 'a'
+	fmt.Printf("%v,%T\n", r1, r1)
+
+	const myConst int = 42
+	fmt.Printf("%v,%T\n", myConst, myConst)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fileSize := 4000000000.
+	fmt.Printf("%.2fGB\n,%.1f", fileSize/GB, 2.)
 }
